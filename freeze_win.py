edb5064f5d101739d8efcfac4cb13cf1e5cf3fa1
dermetti <der.metti@gmail.com>
2024-03-04 16:43:24 +0100

    license update

--- freeze_win.py
+++ freeze_win.py
@@ -4,7 +4,7 @@ PyInstaller.__main__.run([
     'Zeb2Cal_gui.py',
     '--onefile',
     '--windowed',
-    '-n Zeb2Cal-0.1.0',
+    '-n Zeb2Cal-0.1.1',
     '--icon=zeb2cal_icon.ico',
     '--upx-dir=C:/Users/derme/Documents/upx-4.1.0-win64'
     ])
